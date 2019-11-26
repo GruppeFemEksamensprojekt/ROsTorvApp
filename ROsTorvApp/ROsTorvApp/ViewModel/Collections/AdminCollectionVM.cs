af10b8886628475c9bcbd479dfade6825dc79e00
Jakob Hammer <jako274j@edu.easj.dk>
2019-11-26 14:01:17 +0100

    ADMINCOLLECTION

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/AdminCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/AdminCollectionVM.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using ROsTorvApp.Annotations;
 using ROsTorvApp.Model.Users;
 
 namespace ROsTorvApp.ViewModel.Collections
@@ -11,5 +14,11 @@ namespace ROsTorvApp.ViewModel.Collections
     class AdminCollectionVM
     {
         public ObservableCollection<Admin> AdminCollection { get; set; }
+
+        public void AddAdmin(Admin Admin)
+        {
+            AdminCollection.Add(Admin);
+        }
     }
+
 }
