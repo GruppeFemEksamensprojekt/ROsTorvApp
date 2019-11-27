35a6b10e717b70efa1ee507481903e84a2d0f645
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2019-11-27 14:13:39 +0100

    Event model, offer model, ui

--- ROsTorvApp/ROsTorvApp/Model/Center/Offers/Event.cs
+++ ROsTorvApp/ROsTorvApp/Model/Center/Offers/Event.cs
@@ -12,5 +12,12 @@ namespace ROsTorvApp.Model.Center.Offers
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Description { get; set; }
+        public Event(string eventName, DateTime startDate, DateTime endDate, string description)
+        {
+            EventName = eventName;
+            StartDate = startDate;
+            EndDate = endDate;
+            Description = description;
+        }
     }
 }
--- ROsTorvApp/ROsTorvApp/Model/Center/Offers/Offer.cs
+++ ROsTorvApp/ROsTorvApp/Model/Center/Offers/Offer.cs
@@ -8,5 +8,22 @@ namespace ROsTorvApp.Model.Center.Offers
 {
     class Offer
     {
+        public int OfferId { get; set; }
+        public string Description { get; set; }
+        public decimal Discount { get; set; } // i %???
+        public decimal PriceBefore { get; set; }
+        public string Location { get; set; }
+        public DateTime RunTime { get; set; } // Lav om, vi skal bruge start og end date på offer's løbetid
+
+        public Offer(int offerId, string description, decimal discount, decimal priceBefore, string location, DateTime runTime)
+        {
+            OfferId = offerId;
+            Description = description;
+            Discount = discount;
+            PriceBefore = priceBefore;
+            Location = location;
+            RunTime = runTime;
+        }
+    
     }
 }
