9b3c33397267b3914d5856bc7eda077e1ca01945
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2019-11-28 13:30:33 +0100

    navigation

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/CustomerCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/CustomerCollectionVM.cs
@@ -4,21 +4,34 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
 using ROsTorvApp.Helpers;
 using ROsTorvApp.Model.Users;
+using ROsTorvApp.View;
 
 
 namespace ROsTorvApp.ViewModel.Collections
 {
     class CustomerCollectionVM
     {
-        private RelayCommand _NavigateCommand;
+        private RelayCommand _navigateToOpretBrugerCommand;
         public Customer Customer1;
         public ObservableCollection<Customer> CustomerCollection { get; set; }
 
         public CustomerCollectionVM()
         {
+            _navigateToOpretBrugerCommand = new RelayCommand(Navigation, null);
            AddCustomer(Customer1 = new Customer("Customer","Email.com","password","20120100"));
+        }
+
+        public ICommand NavigateCommand
+        {
+            get { return _navigateToOpretBrugerCommand; }
+        }
+
+        private void Navigation()
+        {
         }
 
         public void AddCustomer(Customer customer)
