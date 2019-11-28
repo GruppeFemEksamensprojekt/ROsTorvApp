8fe200b08ce281004f4771ad91bcfe1ca5eab4c8
Jakob Hammer <jako274j@edu.easj.dk>
2019-11-28 13:38:38 +0100

    Event

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/EventCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/EventCollectionVM.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace ROsTorvApp.ViewModel.Collections
 {
     class EventCollectionVM
     {
+        public Event Event1;
         public ObservableCollection<Event> EventCollection { get; set; }
+
+        public EventCollectionVM()
+        {
+            AddEvent(Event1 = new Event("Tilbud In Føtex", new DateTime(2019, 11, 29), 
+                new DateTime(2019,12,07),
+                "Føtex har billig mælk 17.- for økologisk chiafrø mælk!"));
+        }
+
+        public void AddEvent(Event eventevent)
+        {
+            EventCollection.Add(eventevent);
+        }
+
+
+
+
     }
 }
