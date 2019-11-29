0ead367536e6a190a77d13def6523b8a48193d48
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2019-11-29 12:38:04 +0100

    asd

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/StoreCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/StoreCollectionVM.cs
@@ -6,7 +6,10 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using ROsTorvApp.Model.Center;
+using ROsTorvApp.View;
 
 namespace ROsTorvApp.ViewModel.Collections
 {
@@ -27,6 +30,8 @@ namespace ROsTorvApp.ViewModel.Collections
             StoreCollection.Add(new Store(2, "Tøj Eksperten", "08:00 - 15:00", "Tøj Eksperten description!!!", 1, 3, "/Assets/Images/TøjEksperten.jpg", "Tøj","10101010"));
             StoreCollection.Add(new Store(3, "Gamestop+", "08:00 - 15:00", "Gamestop+ description!!!", 1, 4, "/Assets/Images/Gamestop.png", "Gaming","32125341"));
             StoreCollection.Add(new Store(4, "Føtex", "08:00 - 15:00", "Føtex description!!!", 1, 5, "/Assets/Images/Føtex.jpg", "Dagligvarer","95756214"));
+
+            _selectedStore = _storeCollection[0];
         }
 
         #endregion
@@ -39,13 +44,17 @@ namespace ROsTorvApp.ViewModel.Collections
             set { _storeCollection = value; }
         }
         // A property for binding the store you select in the view.
-        public Store SelctedStore
+        public Store SelectedStore
         {
             get { return _selectedStore; }
             set
             {
                 _selectedStore = value;
-                OnPropertyChanged();
+                if (_selectedStore != null)
+                {
+                    ((Frame)Window.Current.Content).Navigate(typeof(StoreDetails));
+                }
+                OnPropertyChanged(nameof(SelectedStore.StoreName));
             }
         }
         #endregion
