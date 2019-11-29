b5e09dedf6f626e24d52173074d3e616283896d6
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2019-11-29 09:53:21 +0100

    Selected store

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/StoreCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/StoreCollectionVM.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ROsTorvApp.Model.Center;
 
 namespace ROsTorvApp.ViewModel.Collections
 {
-   class StoreCollectionVM
-    {
-
+   class StoreCollectionVM : INotifyPropertyChanged
+   {
+        #region Instance Fields
+        private Store _selectedStore;
         private ObservableCollection<Store> _storeCollection;
+        #endregion
+
+        #region Constructor
         public StoreCollectionVM()
         {
             _storeCollection = new ObservableCollection<Store>();
@@ -21,10 +27,35 @@ namespace ROsTorvApp.ViewModel.Collections
             StoreCollection.Add(new Store(3, "Gamestop+", "08:00 - 15:00", "Gamestop+ description!!!", 1, 4, "TestImage", "Gaming"));
             StoreCollection.Add(new Store(4, "Føtex", "08:00 - 15:00", "Føtex description!!!", 1, 5, "TestImage", "Dagligvarer"));
         }
-        public ObservableCollection<Store> StoreCollection 
+
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Store> StoreCollection
         {
             get { return _storeCollection; }
             set { _storeCollection = value; }
         }
+
+        public Store SelctedStore
+        {
+            get { return _selectedStore; }
+            set
+            {
+                _selectedStore = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
