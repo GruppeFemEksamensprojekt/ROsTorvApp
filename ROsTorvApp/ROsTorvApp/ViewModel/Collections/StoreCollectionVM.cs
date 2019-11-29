13580f1bf355f1f6daabd378ab18fdbcd79e3462
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2019-11-29 10:11:07 +0100

    imagestore

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/StoreCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/StoreCollectionVM.cs
@@ -22,7 +22,7 @@ namespace ROsTorvApp.ViewModel.Collections
         {
             _storeCollection = new ObservableCollection<Store>();
             // Test Data
-            StoreCollection.Add(new Store(1, "Matas", "08:00 - 15:00", "Matas description!!!", 1, 2, "TestImage", "Beauty"));
+            StoreCollection.Add(new Store(1, "Matas", "08:00 - 15:00", "Matas description!!!", 1, 2, "Assets//Images//Matas.png", "Beauty"));
             StoreCollection.Add(new Store(2, "Tøj Eksperten", "08:00 - 15:00", "Tøj Eksperten description!!!", 1, 3, "TestImage", "Tøj"));
             StoreCollection.Add(new Store(3, "Gamestop+", "08:00 - 15:00", "Gamestop+ description!!!", 1, 4, "TestImage", "Gaming"));
             StoreCollection.Add(new Store(4, "Føtex", "08:00 - 15:00", "Føtex description!!!", 1, 5, "TestImage", "Dagligvarer"));
