328e84f52b5c6dcfb18fd3f07c0be67ea24ad4c2
Magnus Hansen <magnus131hansen@gmail.com>
2019-12-03 10:30:56 +0100

    File save og user handler burde være færdig

---
+++ ROsTorvApp/ROsTorvApp/Helpers/UserHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ROsTorvApp.Model.Users;
+using ROsTorvApp.ViewModel.Collections;
+
+namespace ROsTorvApp.Helpers
+{
+    class UserHandler
+    {
+        public ObservableCollection<UserAccount> UserList { get; set; }
+
+        public UserHandler()
+        {
+            UserList = new ObservableCollection<UserAccount>();
+            LoadPersonsAsync();
+        }
+        
+        public void UpdateUserList()
+        {
+            foreach (var admin in AdminCollectionVM.AdminCollection)
+            {
+                UserList.Add(admin);
+            }
+
+            foreach (var customer in CustomerCollectionVM.CustomerCollection)
+            {
+                UserList.Add(customer);
+            }
+        }
+
+        public async void SavePersonsAsync()
+        {
+            PersistenceFacade.SaveUserToJson(UserList);
+        }
+
+        public async void LoadPersonsAsync()
+        {
+            ObservableCollection<UserAccount> users = await PersistenceFacade.LoadUserFromJson();
+            UserList.Clear();
+            foreach (var user in users)
+            {
+                UserList.Add(user);
+            }
+        }
+    }
+}
