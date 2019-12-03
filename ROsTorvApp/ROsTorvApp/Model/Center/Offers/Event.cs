dc094c33710fc92f2655a548488f0a38cea74cda
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2019-12-03 11:25:52 +0100

    Events view, implement img til event.cs + start/enddate string property

--- ROsTorvApp/ROsTorvApp/Model/Center/Offers/Event.cs
+++ ROsTorvApp/ROsTorvApp/Model/Center/Offers/Event.cs
@@ -9,15 +9,23 @@ namespace ROsTorvApp.Model.Center.Offers
     class Event
     {
         public string EventName { get; set; }
+        public string EventImage { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Description { get; set; }
-        public Event(string eventName, DateTime startDate, DateTime endDate, string description)
+
+        
+        public Event(string eventName,string eventImg, DateTime startDate, DateTime endDate, string description)
         {
             EventName = eventName;
+            EventImage = eventImg;
             StartDate = startDate;
             EndDate = endDate;
             Description = description;
+        }
+        public string DurationDateText
+        {
+            get { return $"{StartDate.ToString("dd/MM")} - {EndDate.ToString("dd/MM")} - {DateTime.Now.ToString("yyyy")}"; }
         }
     }
 }
