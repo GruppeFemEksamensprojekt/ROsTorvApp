89db7e2f1e392199a6c26104205994c6a08b17fb
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2019-12-05 09:34:43 +0100

    store details delete

--- ROsTorvApp/ROsTorvApp/View/AddStore.xaml.cs
+++ ROsTorvApp/ROsTorvApp/View/AddStore.xaml.cs
@@ -20,9 +20,9 @@ namespace ROsTorvApp.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class StoreDetails : Page
+    public sealed partial class AddStore : Page
     {
-        public StoreDetails()
+        public AddStore()
         {
             this.InitializeComponent();
         }
