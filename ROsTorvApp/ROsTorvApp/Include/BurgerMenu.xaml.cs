1151175ad165dabac553f05bd09fb3b0d1602e3d
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2019-12-06 22:08:04 +0100

    Burgermenu på alle sider, header aligned left, UsersFullName indkorporeret i haeder på mainpage

--- ROsTorvApp/ROsTorvApp/Include/BurgerMenu.xaml.cs
+++ ROsTorvApp/ROsTorvApp/Include/BurgerMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ROsTorvApp.View;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,18 +14,20 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace ROsTorvApp.View
+namespace ROsTorvApp.Include
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class More : Page
+    public sealed partial class BurgerMenu : UserControl
     {
-        public More()
+        public BurgerMenu()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(LoginPage));
         }
     }
 }
