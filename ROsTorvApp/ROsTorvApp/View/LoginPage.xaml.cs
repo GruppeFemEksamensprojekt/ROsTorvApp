40f57a4639c048227326797a90e4ac702f9a2855
Anders Qvistgaard Højer <ande711e@edu.easj.dk>
2019-12-06 11:17:16 +0100

    KNEP DIG SELV

--- ROsTorvApp/ROsTorvApp/View/LoginPage.xaml.cs
+++ ROsTorvApp/ROsTorvApp/View/LoginPage.xaml.cs
@@ -40,5 +40,13 @@ namespace ROsTorvApp.View
                 
             }
         }
+
+        private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((dynamic) this.DataContext).Password = ((PasswordBox) sender).Password;
+            }
+        }
     }
 }
