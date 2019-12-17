703ddca736bbd48f182041f27cee983f49e671fe
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2019-12-17 10:09:51 +0100

    Id

--- ROsTorvApp/ROsTorvApp/ViewModel/Collections/OfferCollectionVM.cs
+++ ROsTorvApp/ROsTorvApp/ViewModel/Collections/OfferCollectionVM.cs
@@ -12,14 +12,14 @@ namespace ROsTorvApp.ViewModel.Collections
         {
             OfferCollection = new ObservableCollection<Offer>();
             AddOffer(new Offer(1, "Matas", "Mandeparfume", 30, 400, "/Assets/Images/Offers/MatasTilbud1.jpg"));
-            AddOffer(new Offer(1, "Matas", "Kvindeparfume", 50, 500, "/Assets/Images/Offers/MatasTilbud2.jpg"));
-            AddOffer(new Offer(1, "GameStop+", "Spil", 60, 349, "/Assets/Images/Offers/GameStopTilbud1.jpg"));
-            AddOffer(new Offer(1, "GameStop+", "Spil", 40, 400, "/Assets/Images/Offers/GameStopTilbud2.jpg"));
-            AddOffer(new Offer(1, "GameStop+", "Spil", 20, 150, "/Assets/Images/Offers/GameStopTilbud3.jpg"));
-            AddOffer(new Offer(1, "GameStop+", "Spilkonsol", 50, 1499, "/Assets/Images/Offers/GameStopTilbud4.jpg"));
-            AddOffer(new Offer(1, "Tøjeksperten", "Jakkesæt", 35, 1500, "/Assets/Images/Offers/ToejEkspertenTilbud1.jpg"));
-            AddOffer(new Offer(1, "Tøjeksperten", "Skjorte", 50, 800, "/Assets/Images/Offers/ToejEkspertenTilbud2.jpg"));
-            AddOffer(new Offer(1, "Tøjeksperten", "Jakke", 50, 2500, "/Assets/Images/Offers/ToejEkspertenTilbud3.jpg"));
+            AddOffer(new Offer(2, "Matas", "Kvindeparfume", 50, 500, "/Assets/Images/Offers/MatasTilbud2.jpg"));
+            AddOffer(new Offer(3, "GameStop+", "Spil", 60, 349, "/Assets/Images/Offers/GameStopTilbud1.jpg"));
+            AddOffer(new Offer(4, "GameStop+", "Spil", 40, 400, "/Assets/Images/Offers/GameStopTilbud2.jpg"));
+            AddOffer(new Offer(5, "GameStop+", "Spil", 20, 150, "/Assets/Images/Offers/GameStopTilbud3.jpg"));
+            AddOffer(new Offer(6, "GameStop+", "Spilkonsol", 50, 1499, "/Assets/Images/Offers/GameStopTilbud4.jpg"));
+            AddOffer(new Offer(7, "Tøjeksperten", "Jakkesæt", 35, 1500, "/Assets/Images/Offers/ToejEkspertenTilbud1.jpg"));
+            AddOffer(new Offer(8, "Tøjeksperten", "Skjorte", 50, 800, "/Assets/Images/Offers/ToejEkspertenTilbud2.jpg"));
+            AddOffer(new Offer(9, "Tøjeksperten", "Jakke", 50, 2500, "/Assets/Images/Offers/ToejEkspertenTilbud3.jpg"));
         }
 
         //A method which adds a new Offer to the list of offers.
